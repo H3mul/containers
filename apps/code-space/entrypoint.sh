59a7f429db920f26ff9b83277fc9146d63649a38
Phillip Dykman <phil.d324@gmail.com>
2024-07-28 09:05:03 -0700

    dd user creation into container

--- apps/code-space/entrypoint.sh
+++ apps/code-space/entrypoint.sh
@@ -2,24 +2,6 @@
 
 set -eu
 
-if id "${USERNAME}" ; then
-    echo "User already exists: ${USERNAME}"
-else
-    echo "Creating new sudo user ${USERNAME}"
-    useradd -G sudo -s ${USER_SHELL} -m "${USERNAME}"
-    passwd -d "${USERNAME}"
-fi
-
 chown "${USERNAME}":"${USERNAME}" -R "/home/${USERNAME}"
-
-if [ ! -z ${CHEZMOI_GH_USER} ]; then
-    sudo -u "${USERNAME}" chezmoi init ${CHEZMOI_GH_USER} --force
-    sudo -u "${USERNAME}" chezmoi state reset
-    sudo -u "${USERNAME}" chezmoi update
-fi
-
-if [ -z ${VS_SERVER_USER} ]; then
-    export VS_SERVER_USER=${USERNAME}
-fi
 
 /usr/bin/supervisord
