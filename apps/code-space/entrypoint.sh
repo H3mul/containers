b425e8d3d98feeee2cb7d54ad094a8ae33e79ec1
Phillip Dykman <phil.d324@gmail.com>
2024-07-28 09:12:51 -0700

    Fix empty home dir skel copy

--- apps/code-space/entrypoint.sh
+++ apps/code-space/entrypoint.sh
@@ -2,6 +2,10 @@
 
 set -eu
 
+if [ -z "$( ls -A "/home/${USERNAME}" )" ]; then
+  cp -a /etc/skel/* "/home/${USERNAME}"
+fi
+  
 chown "${USERNAME}":"${USERNAME}" -R "/home/${USERNAME}"
 
 /usr/bin/supervisord
