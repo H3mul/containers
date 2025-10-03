bf0954270007fffcf5cdecf080568f726cfab47b
Phillip Dykman <phil.d324@gmail.com>
2025-10-02 18:03:02 -0700

    add an ssh server to fluxgym

--- apps/fluxgym/entrypoint.sh
+++ apps/fluxgym/entrypoint.sh
@@ -3,11 +3,40 @@
 set -eu
 
 if [ -d /workspace ]; then
-    if [ ! -d /workspace/models ]; then
-        mv ./models /workspace/models
-    fi
-    rm -rf ./models
+    [ ! -d /workspace/models ] || mv ./models /workspace/models
     ln -s /workspace/models ./models
+
+    [ ! -d /workspace/fluxgym/outputs ] || mkdir -p /workspace/fluxgym/outputs
+    ln -s /workspace/fluxgym/outputs ./outputs
+
+    [ ! -d /workspace/fluxgym/datasets ] || mkdir -p /workspace/fluxgym/datasets
+    ln -s /workspace/fluxgym/datasets ./datasets
+fi
+
+if [[ $PUBLIC_KEY ]]; then
+    echo "Setting up SSH..."
+    mkdir -p ~/.ssh
+    echo -e "${PUBLIC_KEY}\n" >> ~/.ssh/authorized_keys
+    chmod 700 -R ~/.ssh
+
+    if [ ! -f /etc/ssh/ssh_host_rsa_key ]; then
+        ssh-keygen -t rsa -f /etc/ssh/ssh_host_rsa_key -q -N ''
+    fi
+
+    if [ ! -f /etc/ssh/ssh_host_dsa_key ]; then
+        ssh-keygen -t dsa -f /etc/ssh/ssh_host_dsa_key -q -N ''
+    fi
+
+    if [ ! -f /etc/ssh/ssh_host_ecdsa_key ]; then
+        ssh-keygen -t ecdsa -f /etc/ssh/ssh_host_ecdsa_key -q -N ''
+    fi
+
+    if [ ! -f /etc/ssh/ssh_host_ed25519_key ]; then
+        ssh-keygen -t ed25519 -f /etc/ssh/ssh_host_ed25519_key -q -N ''
+    fi
+
+    echo "SSH host keys:"
+    cat /etc/ssh/*.pub
 fi
 
 supervisord -c /etc/supervisord.conf
