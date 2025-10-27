d4365af7048a8a86e2af924cab9d4536b9d1232d
Phillip Dykman <phil.d324@gmail.com>
2025-10-27 10:57:48 -0700

    Add notion-exporter

---
+++ apps/notion-exporter/notion-export.js
@@ -0,0 +1,27 @@
+const notionExporterModule = require("notion-exporter");
+const NotionExporter = notionExporterModule.default;
+
+const fetchEnv = (envName) => {
+  const variable = process.env[envName]?.trim();
+  if (!variable) {
+    throw new Error(`Missing ${envName}`);
+  }
+  return variable;
+};
+
+async function main() {
+  const NOTION_BLOCK_ID = fetchEnv("NOTION_BLOCK_ID");
+  const OUTPUT_DIR = fetchEnv("OUTPUT_DIR");
+
+  await new NotionExporter("", "", {
+    recursive: true,
+    pollInterval: 2000,
+  }).getMdFiles(NOTION_BLOCK_ID, OUTPUT_DIR);
+
+  console.log("Export completed successfully!");
+}
+
+main().catch(error => {
+  console.error("Error:", error);
+  process.exit(1);
+});
