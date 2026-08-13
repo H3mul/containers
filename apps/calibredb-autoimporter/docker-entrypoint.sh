5b30a1133579008e0065760d69f0556aa4dbeecf
Phillip Dykman <phil.d324@gmail.com>
2026-08-13 11:57:16 -0700

    refactor calibredb importer

--- apps/calibredb-autoimporter/docker-entrypoint.sh
+++ apps/calibredb-autoimporter/docker-entrypoint.sh
@@ -2,6 +2,13 @@
 
 CALIBRE_LIBRARY_DIRECTORY=${CALIBRE_LIBRARY_DIRECTORY:-/opt/calibredb/library}
 CALIBREDB_AUTOIMPORT_DIRECTORY=${CALIBREDB_AUTOIMPORT_DIRECTORY:-/opt/calibredb/import}
+CALIBREDB_IMPORT_TIMEOUT=${CALIBREDB_IMPORT_TIMEOUT:-15}
+CALIBREDB_IMPORT_TAG=${CALIBREDB_IMPORT_TAG:-}
+
+CALIBREDB_IMPORT_TAG_CMD=""
+if [ -n "$CALIBREDB_IMPORT_TAG" ]; then
+    CALIBREDB_IMPORT_TAG_CMD="--tag \"$CALIBREDB_IMPORT_TAG\""
+fi
 
 echo "Starting auto-importer process."
 while true
@@ -12,6 +19,6 @@ do
       
       /opt/calibre/calibredb add -r $CALIBREDB_AUTOIMPORT_DIRECTORY \
         --with-library $CALIBRE_LIBRARY_DIRECTORY \
-        --automerge ignore
+        --automerge ignore --timeout 15 $CALIBREDB_IMPORT_TAG_CMD
     fi
 done
